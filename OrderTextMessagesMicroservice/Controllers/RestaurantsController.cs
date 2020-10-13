0c1b6556a236d1b395846ad12aebb55749575529
Veronika Dzham <Lenovo@DESKTOP-0G1U2TE>
2020-10-13 19:35:05 +0300

    create initial structure

---
+++ OrderTextMessagesMicroservice/Controllers/RestaurantsController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrderTextMessagesMicroservice.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class RestaurantsController : ControllerBase
+    {
+       
+    }
+}
