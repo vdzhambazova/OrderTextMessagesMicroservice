1d7e1f8d66af197abba42a2f28ec4ef9fe353b46
Veronika Dzham <Lenovo@DESKTOP-0G1U2TE>
2020-10-14 17:47:38 +0300

    integrate entity framework and sql server db

---
+++ OrderTextMessagesMicroservice/Data/OrderTextMessagesDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace OrderTextMessagesMicroservice.Data
+{
+    public class OrderTextMessagesDbContext : DbContext
+    {
+        public OrderTextMessagesDbContext(DbContextOptions<OrderTextMessagesDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        public DbSet<Order> Orders { get; set; }
+    }
+}
