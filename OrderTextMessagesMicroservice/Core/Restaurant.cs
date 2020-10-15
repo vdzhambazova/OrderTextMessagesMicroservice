fe54678265d1db883ed557aaabb4aaa086e7cf9e
Veronika Dzham <Lenovo@DESKTOP-0G1U2TE>
2020-10-15 09:39:47 +0300

    remove redundant validation

--- OrderTextMessagesMicroservice/Core/Restaurant.cs
+++ OrderTextMessagesMicroservice/Core/Restaurant.cs
@@ -15,14 +15,12 @@ namespace OrderTextMessagesMicroservice
 
         public int Id { get; set; }
 
-        [Required, StringLength(30)]
         public string Name { get; set; }
 
         public string Cuisine { get; set; }
 
         public string Location { get; set; }
 
-        [Required, StringLength(10)]
         public string DeliveryTime { get; set; }
     }
 }
