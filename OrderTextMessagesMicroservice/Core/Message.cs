3832710c31017b722a101361153d9c64f09e5327
Veronika Dzham <Lenovo@DESKTOP-0G1U2TE>
2020-10-15 09:19:55 +0300

    change models location

---
+++ OrderTextMessagesMicroservice/Core/Message.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderTextMessagesMicroservice
+{
+    public class Message
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(30)]
+        public string RestaurantName { get; set; }
+
+        [Required, StringLength(10)]
+        public string DeliveryTime { get; set; }
+    }
+}
